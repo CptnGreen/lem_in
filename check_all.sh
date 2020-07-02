d03ae4a80362ed4367fd8f3c0a71bc79fed31a35
CptnGreen <a.krasnoshchekov1992@gmail.com>
2020-07-02 20:18:12 +0300

    Alright

--- check_all.sh
+++ check_all.sh
@@ -12,7 +12,6 @@ echo -e ""
 grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
 grep -c '^L[0-9]' < temp
 
-exit 1
 
 echo -e "\n====Ten ants to one path========\n"
 ./lemin-gen --flow-ten > input
