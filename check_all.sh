578eb6bf141599d9c8ccaaacfaca74e4f20fab65
CptnGreen <a.krasnoshchekov1992@gmail.com>
2020-07-02 18:45:22 +0300

    Uncommented all tests

--- check_all.sh
+++ check_all.sh
@@ -13,56 +13,56 @@ grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines re
 grep -c '^L[0-9]' < temp
 
 
-# echo -e "\n====Ten ants to one path========\n"
-# lemin-gen --flow-ten > input
-# echo -e "Time:"
-# time ./lem-in < input > temp
-# echo -e ""
-# echo -e "Leaks:"
-# valgrind --log-file=leaks_log ./lem-in < input > /dev/null
-# awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
-# echo -e ""
-# rg '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
-# rg -c '^L[0-9]' < temp
+echo -e "\n====Ten ants to one path========\n"
+lemin-gen --flow-ten > input
+echo -e "Time:"
+time ./lem-in < input > temp
+echo -e ""
+echo -e "Leaks:"
+valgrind --log-file=leaks_log ./lem-in < input > /dev/null
+awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
+echo -e ""
+grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
+grep -c '^L[0-9]' < temp
 
 
-# echo -e "\n====Thousand ants to one path===\n"
-# lemin-gen --flow-thousand > input
-# echo -e "Time:"
-# time ./lem-in < input > temp
-# echo -e ""
-# echo -e "Leaks:"
-# valgrind --log-file=leaks_log ./lem-in < input > /dev/null
-# awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
-# echo -e ""
-# rg '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
-# rg -c '^L[0-9]' < temp
+echo -e "\n====Thousand ants to one path===\n"
+lemin-gen --flow-thousand > input
+echo -e "Time:"
+time ./lem-in < input > temp
+echo -e ""
+echo -e "Leaks:"
+valgrind --log-file=leaks_log ./lem-in < input > /dev/null
+awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
+echo -e ""
+grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
+grep -c '^L[0-9]' < temp
 
 
-# echo -e "\n====Big (~4K rooms) map=========\n"
-# lemin-gen --big > input
-# echo -e "Time:"
-# time ./lem-in < input > temp
-# echo -e ""
-# echo -e "Leaks:"
-# valgrind --log-file=leaks_log ./lem-in < input > /dev/null
-# awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
-# echo -e ""
-# rg '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
-# rg -c '^L[0-9]' < temp
+echo -e "\n====Big (~4K rooms) map=========\n"
+lemin-gen --big > input
+echo -e "Time:"
+time ./lem-in < input > temp
+echo -e ""
+echo -e "Leaks:"
+valgrind --log-file=leaks_log ./lem-in < input > /dev/null
+awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
+echo -e ""
+grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
+grep -c '^L[0-9]' < temp
 
 
-# echo -e "\n====Complex big map=============\n"
-# lemin-gen --big-superposition > input
-# echo -e "Time:"
-# time ./lem-in < input > temp
-# echo -e ""
-# echo -e "Leaks:"
-# valgrind --log-file=leaks_log ./lem-in < input > /dev/null
-# awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
-# echo -e ""
-# rg '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
-# rg -c '^L[0-9]' < temp
+echo -e "\n====Complex big map=============\n"
+lemin-gen --big-superposition > input
+echo -e "Time:"
+time ./lem-in < input > temp
+echo -e ""
+echo -e "Leaks:"
+valgrind --log-file=leaks_log ./lem-in < input > /dev/null
+awk -F"==     " ' /in use at exit/{ print $2 } ' < leaks_log
+echo -e ""
+grep '#Here' < temp | uniq | awk -F": " ' { printf "Lines required: %s\nLines received: ", $2 } '
+grep -c '^L[0-9]' < temp
 
 
 echo -e "\n================================"
